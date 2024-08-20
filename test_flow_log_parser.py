2ccc3eeeabef8c25ba67eac0dd7a201eb23e1485
= <rishitpallav9@gmail.com>
2024-08-20 16:24:47 -0500

    Updated path locations with variables for better readability.

--- test_flow_log_parser.py
+++ test_flow_log_parser.py
@@ -1,4 +1,3 @@
-import os
 from flow_log_parser import FlowLogParser
 
 def compare_files (file1, file2):
@@ -10,28 +9,43 @@ def compare_files (file1, file2):
 
 def test_flow_log_parser():
 
+    # Test file locations
+    flow_logs_test1_path = 'tests/flow_logs_test1.txt'
+    lookup_table_test1_path = 'tests/lookup_table_test1.csv'
+    matches_tag_test1_path = 'tests/matches_tag_test1.txt'
+    matches_port_protocol_test1_path = 'tests/matches_port_protocol_test1.txt'
+
+    flow_logs_test2_path = 'tests/flow_logs_test2.txt'
+    lookup_table_test2_path = 'tests/lookup_table_test2.csv'
+    matches_tag_test2_path = 'tests/matches_tag_test2.txt'
+    matches_port_protocol_test2_path = 'tests/matches_port_protocol_test2.txt'
+
+
+
     # TEST 1
-    parser = FlowLogParser('tests/flow_logs_test1.txt', 'tests/lookup_table_test1.csv')
+    parser = FlowLogParser(flow_logs_test1_path, lookup_table_test1_path)
     parser.parse_lookup_table()
     parser.parse_flow_logs()
     parser.output_results()
 
     try:
-        assert compare_files('tests/matches_tag_test1.txt', 'matches_tag.txt')
-        assert compare_files('tests/matches_port_protocol_test1.txt', 'matches_port_protocol.txt')
+        assert compare_files(matches_tag_test1_path, 'matches_tag.txt')
+        assert compare_files(matches_port_protocol_test1_path, 'matches_port_protocol.txt')
+        print("TEST 1 PASSED")
     except AssertionError:
         print("TEST 1 FAILED. Make sure the expected output file has new line at the End Of File.")
         return False
 
     # TEST 2
-    parser = FlowLogParser('tests/flow_logs_test2.txt', 'tests/lookup_table_test2.csv')
+    parser = FlowLogParser(flow_logs_test2_path, lookup_table_test2_path)
     parser.parse_lookup_table()
     parser.parse_flow_logs()
     parser.output_results()
 
     try:
-        assert compare_files('tests/matches_tag_test2.txt', 'matches_tag.txt')
-        assert compare_files('tests/matches_port_protocol_test2.txt', 'matches_port_protocol.txt')
+        assert compare_files(matches_tag_test2_path, 'matches_tag.txt')
+        assert compare_files(matches_port_protocol_test2_path, 'matches_port_protocol.txt')
+        print("TEST 2 PASSED")
     except AssertionError:
         print("TEST 2 FAILED. Make sure the expected output file has new line at the End Of File.")
         return False
